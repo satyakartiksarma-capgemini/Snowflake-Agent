c9ce85b7d3d3ed70d5a586ac5495bc6e683c8600
Gollakota <satya-kartik-sarma.gollakota@capgemini.com>
2025-12-10 16:17:36 +0530

    3 - commit

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,17 @@
 /// <reference types="vite/client" />
+ 
+interface ImportMetaEnv {
+  readonly VITE_SF_BASE_URL: string;
+  readonly VITE_SF_DATABASE: string;
+  readonly VITE_SF_SCHEMA: string;
+  readonly VITE_SF_WAREHOUSE: string;
+  readonly VITE_SF_AGENT_DEFAULT: string;
+  readonly VITE_SF_AGENT_HUMAN: string;
+  readonly VITE_SF_AGENT_AUTO: string;
+  readonly VITE_SF_BEARER_TOKEN: string;
+  // add more variables as needed
+}
+ 
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
