897def726c1259a344ea438e362454d8aa8a733c
Gollakota <satya-kartik-sarma.gollakota@capgemini.com>
2025-12-10 15:29:24 +0530

    3 - commit

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/app/",
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
